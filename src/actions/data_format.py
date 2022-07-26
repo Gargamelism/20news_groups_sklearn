2bd00095458e2e179a8b060d55e800e844beb24d
Shir-Lee Kimelman <shirlee.kimelman@gmail.com>
2022-07-26 18:45:24 +0300

    solved your problem PERIOD!

--- src/actions/data_format.py
+++ src/actions/data_format.py
@@ -1,17 +1,10 @@
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
-import dataclasses
-from typing import Callable, List, Any
 
 from data_classes import ParsedArgs
 from helpers.functional import identity
 
-@dataclasses.dataclass
-class DataFormatterWrapper:
-    count_vectorizer: Any
-    tfidf: Any
-    format: Callable[[List[Any]], List[Any]]
 
-def get_formatter_func(formatter_name: str) -> DataFormatterWrapper:
+def get_formatter_func(formatter_name: str):
     if not formatter_name:
         return identity
 
@@ -26,11 +19,9 @@ def tfidf_formatter(data):
     tfidf_transformer.fit(vectorized_data)
 
     data_formatter = lambda data: tfidf_transformer.transform(count_vectorizer.transform(data))
-    return [count_vectorizer, tfidf_transformer, data_formatter]
+    return data_formatter
 
 
-def get_data_formatter_wrapper(parsed_args: ParsedArgs, data) -> DataFormatterWrapper:
-    data_formatter_func = get_formatter_func(parsed_args.data_format)
-    [count_vectorizer, tfidf, data_formatter] = data_formatter_func(data)
-    data_formatter_wrapper = DataFormatterWrapper(count_vectorizer, tfidf, data_formatter)
-    return data_formatter_wrapper
+def get_data_formatter(parsed_args: ParsedArgs, data):
+    data_formatter = get_formatter_func(parsed_args.data_format)
+    return data_formatter(data)
---
+++ src/tmp/data_processing.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_20newsgroups
+import re
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+class DataPreprocessing:
+
+    @classmethod
+    def get_data(cls):
+
+        # Read
+        data = fetch_20newsgroups(subset="all")
+        df = pd.DataFrame({'text': data.data, 'topic': data.target})
+
+        # Distribution / balancy
+        print(df.shape)
+        print(df['topic'].value_counts(normalize=True))
+        # df.groupby('topic').count().plot.bar()
+        # plt.show()
+
+        return df
+
+    @classmethod
+    def clean(cls, text, stemming=True):
+
+        # Remove symbols (?!., etc.) - keep only words and numbers
+        text = re.sub('[^a-zA-Z0-9]', ' ', text)
+        # If number, indicate a number
+        text = re.sub('[0-9]+', 'number', text)
+        # Uncapitalize
+        text = text.lower()
+
+        # Split
+        text = text.split()
+        # Stopwords removal
+        stop = stopwords.words('english')
+        text = [word for word in text if (word not in set(stop))]
+        # Stem
+        if stemming:
+            snowball = SnowballStemmer("english")
+            text = [snowball.stem(word) for word in text]  # stemming
+
+        # Return a list of words
+        text = ' '.join(text)
+        return text
+
+    @classmethod
+    def features(cls, X_train, X_valid, X_test):
+
+        # Vectorizer
+        vect = TfidfVectorizer(ngram_range=(1, 2), max_features=500)
+
+        # Fit and transform
+        X_train = vect.fit_transform(X_train).toarray()
+        X_valid = vect.transform(X_valid).toarray()
+        X_test = vect.transform(X_test).toarray()
+
+        # Feature names
+        X_train = pd.DataFrame(data=X_train, columns=vect.get_feature_names_out())
+        X_valid = pd.DataFrame(data=X_valid, columns=vect.get_feature_names_out())
+        X_test = pd.DataFrame(data=X_test, columns=vect.get_feature_names_out())
+
+        return X_train, X_valid, X_test
